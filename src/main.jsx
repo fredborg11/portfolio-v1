c994d6c5c40acb64f3cb7d9108a29cdcdb3a8b92
Thomas Fredborg Nielsen <thomasmacbookair@MacBookAir.home>
2025-12-27 12:10:32 +0100

    Added gallery route

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 import App from './App.jsx'
 import Gallery from './sections/Gallery.jsx'   // <-- vigtig
 import './index.css'
