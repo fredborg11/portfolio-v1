618584e0a53c11fa4dce6100c2f8929dfd6da8ce
Thomas Fredborg Nielsen <thomasmacbookair@MacBookAir.home>
2025-12-27 11:49:50 +0100

    Added cloudinary images to DomeGallery

--- src/components/DomeGallery.jsx
+++ src/components/DomeGallery.jsx
@@ -4,32 +4,32 @@ import { useGesture } from '@use-gesture/react';
 
 const DEFAULT_IMAGES = [
   {
-    src: 'https://images.unsplash.com/photo-1755331039789-7e5680e26e8f?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    alt: 'Abstract art'
+    src: 'https://res.cloudinary.com/dnkqhm27a/image/upload/v1766829181/pinetree_image_3_glddt8.webp',
+    alt: 'Pinetree Golf, Logo and Packaging Design'
   },
   {
-    src: 'https://images.unsplash.com/photo-1755569309049-98410b94f66d?q=80&w=772&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    alt: 'Modern sculpture'
+    src: 'https://res.cloudinary.com/dnkqhm27a/image/upload/v1766829330/pinetree_image_2_ietxhy.webp',
+    alt: 'Pinetree Golf, Logo and Packaging Design'
   },
   {
-    src: 'https://images.unsplash.com/photo-1755497595318-7e5e3523854f?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    alt: 'Digital artwork'
+    src: 'https://res.cloudinary.com/dnkqhm27a/image/upload/v1766829792/trope-%C3%B8ko-dadler-milj%C3%B8-1_y0a6mf.webp',
+    alt: 'The Whole Company, Packaging Design'
   },
   {
-    src: 'https://images.unsplash.com/photo-1755353985163-c2a0fe5ac3d8?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    alt: 'Contemporary art'
+    src: 'https://res.cloudinary.com/dnkqhm27a/image/upload/v1766830385/ChatGPT_Image_27._dec._2025_11.08.32_ezfwkn.webp',
+    alt: 'The Whole Company, Packaging Design'
   },
   {
-    src: 'https://images.unsplash.com/photo-1745965976680-d00be7dc0377?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    alt: 'Geometric pattern'
+    src: 'https://res.cloudinary.com/dnkqhm27a/image/upload/v1766830770/ChatGPT_Image_27._dec._2025_11.18.43_mucwgs.webp',
+    alt: 'Pisiffik, UI/UX Design'
   },
   {
-    src: 'https://images.unsplash.com/photo-1752588975228-21f44630bb3c?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    alt: 'Textured surface'
+    src: 'https://res.cloudinary.com/dnkqhm27a/image/upload/v1766831455/Sk%C3%A6rmbillede_2025-12-27_kl._11.21.18_xkd1mf.png',
+    alt: 'Unlimit Group, Webdesign'
   },
   {
-    src: 'https://pbs.twimg.com/media/Gyla7NnXMAAXSo_?format=jpg&name=large',
-    alt: 'Social media image'
+    src: 'https://res.cloudinary.com/dnkqhm27a/image/upload/v1766832373/ChatGPT_Image_27._dec._2025_11.45.39_eklinq.webp',
+    alt: 'FLID, Trykte medier'
   }
 ];
 
